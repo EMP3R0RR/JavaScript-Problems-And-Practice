dba29e0377cd6e2a223a578cceae8d2fa2d4c595
EMP3R0RR <syedpunno@gmail.com>
2025-11-03 23:22:10 +0600

    Add prompt-sync and ansi-regex for user input and ANSI code handling

--- Prob2.js
+++ Prob2.js
@@ -7,3 +7,26 @@ for (let i = 1; i <= rows; i++) {
     }
     console.log(line);
 }
+
+//The Slightly Easier Node Option (prompt-sync) o take user input from he console using prompt. install command: npm install prompt-sync. 
+
+//Find the factorial of a given number (e.g., 5! = 120).
+const prompt = require('prompt-sync')();
+let  n=prompt("Please enter your number:");
+let fact = 1;
+
+for (let i = 1; i <= n; i++) {
+    fact *= i;
+}
+console.log("Factorial:", fact);
+
+//Sum of digits of a number (e.g., 123 -> 1 + 2 + 3 = 6).
+let num = ("Enter a number:");
+let sum = 0;
+
+while (num > 0) {
+    sum += num % 10;
+    num = Math.floor(num / 10);
+}
+
+console.log("Sum of digits:", sum);
