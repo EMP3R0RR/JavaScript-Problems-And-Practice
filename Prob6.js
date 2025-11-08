0ebfa027e50590ac566dac639e00eafaba129f11
EMP3R0RR <syedpunno@gmail.com>
2025-11-08 23:51:51 +0600

    Add Fibonacci sequence generator implementation

---
+++ Prob6.js
@@ -0,0 +1,14 @@
+//Generate Fibonacci sequence
+
+let n = parseInt(prompt("Enter how many Fibonacci numbers:"));
+let a = 0, b = 1;
+
+console.log(a);
+console.log(b);
+
+for (let i = 2; i < n; i++) {
+    let next = a + b;
+    console.log(next);
+    a = b;
+    b = next;
+}
